d1820977a8e1cf5fc8690058bb2521a7b4ced769
LordSven <bussbilen@hotmail.com>
2025-09-12 10:16:58 +0200

    Added Lance.py through git

---
+++ Egna-projekt/Lance.py
@@ -0,0 +1,14 @@
+name = input("What's her name? ")
+print(f"Listen here, {name}!")
+print(f"{name}?")
+name2 = input(f"What the FUCK, did I just hear {name}!? ")
+print(f"{name2}! I said {name2}, I'm not gay.")
+age = input(f"How old is this {name2}? ")
+if int(age) >= 40: 
+    print(f"{age} is a bit old innit?")
+elif int(age) <= 16:
+    print('0.o')
+elif int(age) == 17:
+    print('Oh you like living on the edge, huh.')
+else:
+    print(f"{age} is a good age.")
